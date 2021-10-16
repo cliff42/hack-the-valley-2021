0ab981c3f7b600cfeb748ccffeec83f0758c3913
Alexander Maximovitch <maximdim@student.ubc.ca>
2021-10-16 14:04:42 -0700

    Changes to app

--- server/app.js
+++ server/app.js
@@ -11,6 +11,14 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  next();
+  });
+
 // Gets a list of the current open events from NASA's EONET API
 app.get("/events", (req, res) => {
   // NASA's EONET API

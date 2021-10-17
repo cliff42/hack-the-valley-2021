37855da23e014b38d8913623c3c8cf4cfe8a91d4
cliff42 <chris.cliff@shaw.ca>
2021-10-16 23:08:45 -0600

    added mongodb functionality

--- client/src/app/account/account.component.ts
+++ client/src/app/account/account.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
 
@@ -8,14 +9,33 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 })
 export class AccountComponent implements OnInit {
 
-  constructor(public auth: AngularFireAuth) { }
+  constructor(public auth: AngularFireAuth, private http:HttpClient) { }
 
   ngOnInit(): void {
   }
 
   todayDate = new Date();
 
-  getPhoneInput(phoneNum: string) {
+  sendPhoneInput(userId: string, phoneNum: string) {
+    console.log(userId);
     console.log(phoneNum);
+    var formData = {
+      "uid": userId,
+      "phoneNumber": phoneNum
+    }
+    let res = this.http.post<any>('http://localhost:3000/add-user', formData);
+    res.subscribe((data) => {
+      this.sendTestMessage(userId);
+    });
+  }
+
+  sendTestMessage(userId: string): void {
+    var formData = {
+      "uid": userId
+    }
+    let res = this.http.post<any>('http://localhost:3000/send-message', formData);
+    res.subscribe((data) => {
+      console.log(data.message);
+    });
   }
 }
--- server/app.js
+++ server/app.js
@@ -17,6 +17,11 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+// HTTP requests
+const bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}) );
+
 mongoose.connect(dbConfig.url, {
     useNewUrlParser: true, 
     useUnifiedTopology: true 
@@ -38,7 +43,7 @@ app.use(function (req, res, next) {
 // Gets a list of the current open events from NASA's EONET API
 app.get("/events", (req, res) => {
   // NASA's EONET API
-  axios.get('https://eonet.sci.gsfc.nasa.gov/api/v3/events', { params: { status: "open", limit: 100 } })
+  axios.get('https://eonet.sci.gsfc.nasa.gov/api/v3/events', { params: { status: "open", limit: 250 } })
     .then(response => {
       console.log('getting NASA EONET event data...');
       res.json({ message: response.data.events });
@@ -49,27 +54,35 @@ app.get("/events", (req, res) => {
 });
 
 // Send a message to the user's phone number (from Google Firebase) using Twilio
-app.get("/send-message", (req, res) => {
-  if (req.number != null) {
-    client.messages
-    .create({
-        body: 'There is a natural event warning in your area',
-        from: '+13514443957',
-        to: req.number
-     })
-    .then(message => {
-      console.log(message.sid);
-      res.json({message: message.sid});
-    });
-  } else {
-    res.json({message: 'There is no phone number on record for your google account'});
-  }
+app.post("/send-message", async (req, res) => {
+    try {
+        let user = await User.find({uid: req.body.uid});
+        if (user.length > 0) {
+            client.messages
+            .create({
+                body: 'Test for Natural Events',
+                from: '+13514443957',
+                to: user[0].phoneNumber
+             })
+            .then(message => {
+              console.log("sent message" + message.sid);
+              res.json({message: message.sid});
+            });
+          } else {
+            res.json({message: 'There is no phone number on record for your google account'});
+          }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send(err);
+    }
 });
 
-app.get("/test-db", async (req, res) => {
+app.post("/add-user", async (req, res) => {
+    let user = new User({ uid: req.body.uid, phoneNumber: req.body.phoneNumber });
     try {
-        let users = await User.find();
-        res.status(200).send(users);
+        await user.save()
+        console.log("added phone number for user: " + req.body.uid);
+        res.status(200).send(user);
     } catch (err) {
         console.error(err);
         res.status(500).send(err);
